2b07eacb218dcfe58ad0519d7329d378fa2d260b
Lexane777 <Lexane777@gmail.com>
2024-04-23 12:55:59 +0400

    New task

---
+++ task1.6.py
@@ -0,0 +1,6 @@
+import math
+print("Программа выводит степень числа ")
+floatNumber1=float (input("Введите число "))
+floatNumber2=float (input("Введите степень числа"))
+result=pow(floatNumber1,floatNumber2)
+print(f"степень числа {floatNumber1} соответствует {result}")
