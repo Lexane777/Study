067a873f42b8ddd8ff28572843f34ccd1f45d617
Lexane777 <Lexane777@gmail.com>
2024-04-23 14:13:16 +0400

    New task

---
+++ Practice1.py
@@ -0,0 +1,10 @@
+listFindMax=[]
+intNumber1=int (input("Введите первое число "))
+listFindMax.append(intNumber1)
+intNumber2=int (input("Введите второе число "))
+listFindMax.append(intNumber2)
+intNumber3=int (input("Введите третье число "))
+listFindMax.append(intNumber3)
+print(f"Максимальное число: {max(listFindMax)}")
+
+
